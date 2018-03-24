b381fa59ac4c60461f61b30fe03f1e27a44dff39
eng.Anton Anchev <robobyala@yahoo.com>
2018-03-24 18:00:26 +0200

    Add files via upload

---
+++ Button_led_ON_OFF/Button_led_ON_OFF.ino
@@ -0,0 +1,21 @@
+const int buttonPin = 12;     // the number of the pushbutton pin
+const int ledPin =  13;       // the number of the LED pin
+int buttonState = 0;          // variable for reading the pushbutton status
+
+// ================= SETUP =================
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+// ================ LOOP ===================
+void loop() {
+
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);       // turn LED on:
+  }
+  else {
+    digitalWrite(ledPin, LOW);        // turn LED off:
+  }
+}
