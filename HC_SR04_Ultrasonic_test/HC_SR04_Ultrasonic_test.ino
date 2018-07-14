dc46c2d2fa73f24493911a53b8f5fbc2e884531f
eng.Anton Anchev <robobyala@yahoo.com>
2018-07-14 23:56:43 +0300

    Add files via upload

---
+++ HC_SR04_Ultrasonic_test/HC_SR04_Ultrasonic_test.ino
@@ -0,0 +1,18 @@
+
+#include <Ultrasonic.h>
+Ultrasonic ultrasonic(7, 8);
+
+// ================== SETUP ======================
+void setup()
+{
+  Serial.begin(9600);
+}
+
+// ================= LOOP =======================
+void loop()
+{
+  int distance = ultrasonic.distanceRead();
+  Serial.println(distance);
+  delay(10);
+
+}
