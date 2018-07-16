a354d3ca3f76ef9d200658af50dabf5bdaef0c6b
eng.Anton Anchev <robobyala@yahoo.com>
2018-07-16 15:59:02 +0300

    Add files via upload

---
+++ buzzer_melody_RedDragon/buzzer_melody_RedDragon.ino
@@ -0,0 +1,34 @@
+/*
+  Melody with buzzer
+  8-ohm speaker on digital pin 11
+*/
+#include "pitches.h"                // файл с нотите
+#define buzzer 11                   // пин на зумера
+
+// ---------- ноти в мелодията в масив
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+// ---------- дължина на нотите -> 4 = четвъртина нота, 8 = осмина нота, и т.н.
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+// ======================= SETUP =========================
+void setup() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    // ------------- изчисляване на нотите -> четвъртина нота = 1000 / 4, осмина нота = 1000/8 , и т.н..
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(buzzer, melody[thisNote], noteDuration);
+    digitalWrite(buzzer, HIGH);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(buzzer);                    // спиране на тоновете
+  }
+  digitalWrite(buzzer, HIGH);        //поставяме пина в HIGH,за да спре да свири
+}
+
+// ====================== LOOP ==========================
+void loop() {
+
+}
